c7cb37b06fdee344a01308afa37ef0d199d5a268
vinceclave <datanaganvince351@gmail.com>
2025-09-01 19:39:31 +0800

    mobile boilerplate

---
+++ apps/mobile/App.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-purple-500">
+      <Text className="text-white text-2xl font-bold">
+        Hello NativeWind!
+      </Text>
+    </View>
+  );
+}
