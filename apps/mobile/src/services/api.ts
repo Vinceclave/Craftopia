565a7f15d924e5357cac197c806a6b635dbc4610
vinceclave <datanaganvince351@gmail.com>
2025-09-01 18:49:40 +0800

    revoke

--- apps/mobile/src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_BASE_URL = 'http://192.168.1.10/api'; // replace with your backend URL
-
-export const postGoogleToken = async (idToken: string) => {
-  const response = await axios.get(`${API_BASE_URL}/auth/google/`);
-  return response.data;
-};
