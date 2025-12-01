06c0fae5c0a1c649f9eaf173ad0977e495e812cf
vinceclave <datanaganvince351@gmail.com>
2025-12-02 00:04:15 +0800

    feat: add initial landing page with banner component

--- apps/web/src/pages/Landing.tsx
+++ apps/web/src/pages/Landing.tsx
@@ -1,6 +1,6 @@
 import { Banner } from '@/components/home/Banner'
-import { Features } from '@/components/home/Features'
-import { HowItWorks } from '@/components/home/HowItWork'
+// import { Features } from '@/components/home/Features'
+// import { HowItWorks } from '@/components/home/HowItWork'
 
 export const Landing = () => {
   return (
